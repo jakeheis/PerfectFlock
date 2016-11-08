059e78766e259c68d90649602322e9ce644d1949
Jake Heiser <jakeheiser1@gmail.com>
2016-11-07 23:06:04 -0800

    Trying new system

--- Sources/PerfectFlock.swift
+++ Sources/PerfectFlock.swift
@@ -2,12 +2,7 @@ import Flock
 import Foundation
 
 public extension Flock {
-    static let Perfect: [Task] = [
-        ToolsTask(),
-        StopTask(),
-        StartTask(),
-        PsTask()
-    ]
+    static let Perfect: [Task] = SystemdTasks(provider: PerfectSystemdProvder()).createTasks() + [EnvTask(), ToolsTask()]
 }
 
 public extension Config {
@@ -17,12 +12,74 @@ public extension Config {
     static var root: String? = nil
     static var serverName: String? = nil
     static var runAs: String? = nil
-    
-    static var outputLog = "/dev/null"
-    static var errorLog = "/dev/null"
 }
 
 let perfect = "perfect"
+let confFilePath = "/etc/perfect.conf"
+
+public class PerfectSystemdProvder: SystemdProvider {
+    
+    static var argumentString = ""
+    
+    public let name = perfect
+    public let serviceName = perfect
+    
+    public var serviceFileContents: String {
+        return [
+            "[Unit]",
+            "Description=\"The \(name) server\"",
+            "",
+            "[Service]",
+            "EnvironmentFile=\(confFilePath)",
+            "ExecStart=\"\(Paths.executable)\" \(PerfectSystemdProvder.argumentString)",
+            "Restart=on-failure",
+            ""
+        ].joined(separator: "\n")
+    }
+}
+
+public class EnvTask: Task {
+    public let name = "env"
+    public let namespace = perfect
+    public let hookTimes: [HookTime] = [.before("perfect:restart")]
+    
+    public func run(on server: Server) throws {        
+        var lines: [String] = []
+        var keys: [String] = []
+        
+        func add(_ key: String, _ value: String) {
+            lines.append("\(key)=\(value)")
+            keys.append(key)
+        }
+        
+        if let ssl = Config.ssl {
+            add("SSH", "--sslcert \(ssl.sslCert) --sslkey \(ssl.sslKey)")
+        }
+        if let port = Config.port {
+            add("PORT", "--port \(port)")
+        }
+        if let address = Config.address {
+            add("ADDRESS", "--address \(address)")
+        }
+        if let root = Config.root {
+            add("ROOT", "--root \(root)")
+        }
+        if let serverName = Config.serverName {
+            add("NAME", "--name \(serverName)")
+        }
+        if let runAs = Config.runAs {
+            add("RUNAS", "--runas \(runAs)")
+        }
+        
+        let config = lines.joined(separator: "\n")
+        try server.execute("echo \"\(config)\" > \(confFilePath)")
+        
+        PerfectSystemdProvder.argumentString = keys.map({ "$" + $0 }).joined(separator: " ")
+        
+        try invoke("perfect:write-service") // Always rewrite service to update ExecStart
+    }
+    
+}
 
 public class ToolsTask: Task {
     public let name = "tools"
@@ -33,80 +90,4 @@ public class ToolsTask: Task {
         print("Installing Perfect dependencies")
         try server.execute("sudo apt-get -qq install openssl libssl-dev uuid-dev")
     }
-}
-
-public class StopTask: Task {
-    public let name = "stop"
-    public let namespace = perfect
-    public let hookTimes: [HookTime] = [.before("deploy:link")]
-    
-    public func run(on server: Server) throws {
-        if let pid = try findServerPid(on: server) {
-            try server.execute("kill -9 \(pid)")
-        } else {
-            print("Perfect not running")
-        }
-    }
-}
-
-public class StartTask: Task {
-    public let name = "start"
-    public let namespace = perfect
-    public let hookTimes: [HookTime] = [.after("deploy:link")]
-    
-    public func run(on server: Server) throws {
-        print("Starting Perfect")
-        var execComponents = [Paths.executable]
-        if let ssl = Config.ssl {
-            execComponents += ["--sslcert \(ssl.sslCert)"]
-            execComponents += ["--sslkey \(ssl.sslKey)"]
-        }
-        if let port = Config.port {
-            execComponents += ["--port \(port)"]
-        }
-        if let address = Config.address {
-            execComponents += ["--address \(address)"]
-        }
-        if let root = Config.root {
-            execComponents += ["--root \(root)"]
-        }
-        if let serverName = Config.serverName {
-            execComponents += ["--name \(serverName)"]
-        }
-        if let runAs = Config.runAs {
-            execComponents += ["--runas \(runAs)"]
-        }
-        let execString = execComponents.joined(separator: " ")
-        try server.execute("nohup \(execString) >> \(Config.outputLog) 2>> \(Config.errorLog) &")
-        try invoke("perfect:ps")
-    }
-}
-
-public class PsTask: Task {
-    public let name = "ps"
-    public let namespace = perfect
-    
-    public func run(on server: Server) throws {
-        if let pid = try findServerPid(on: server) {
-            print("Perfect running as process \(pid)")
-        } else {
-            print("Perfect not running")
-        }
-    }
-}
-
-private func findServerPid(on server: Server) throws -> String? {
-    guard let processes = try server.capture("ps aux | grep \".build\"") else {
-        return nil
-    }
-    
-    let lines = processes.components(separatedBy: "\n")
-    for line in lines where !line.contains("grep") {
-        let segments = line.components(separatedBy: " ").filter { !$0.isEmpty }
-        if segments.count > 1 {
-            return segments[1]
-        }
-        return segments.count > 1 ? segments[1] : nil
-    }
-    return nil
 }
