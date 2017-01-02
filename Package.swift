0e28674b95e81a1a72c23883cc966eb45f6f6db7
Jake Heiser <jakeheiser1@gmail.com>
2017-01-02 08:38:20 -0600

    LOcal

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "PerfectFlock",
     dependencies: [
-        .Package(url: "https://github.com/jakeheis/Flock", majorVersion: 0, minor: 1)
+        .Package(url: "/Users/jakeheiser/Desktop/Projects/Swift/Flock", majorVersion: 0, minor: 1)
     ]
 )
