b9edf2c9ca4ed63de84c4718cf91a4393da5f026
Jake Heiser <jakeheiser1@gmail.com>
2017-07-15 18:08:36 -0500

    Added deprecation

--- Package.swift
+++
@@ -1,8 +0,0 @@
-import PackageDescription
-
-let package = Package(
-    name: "PerfectFlock",
-    dependencies: [
-        .Package(url: "/Users/jakeheiser/Desktop/Projects/Swift/Flock", majorVersion: 0, minor: 1)
-    ]
-)
--- Tests/LinuxMain.swift
+++
@@ -1,6 +0,0 @@
-import XCTest
-@testable import PerfectFlockTests
-
-XCTMain([
-     testCase(PerfectFlockTests.allTests),
-])
